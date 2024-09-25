c71ba7ccbb17933b63c89fa8e28b86fc5f07e928
christophekede <chris.kede@yahoo.fr>
2024-09-26 00:19:12 +0200

    Settings project

---
+++ index.js
@@ -0,0 +1,14 @@
+const Lexer = require("./Lexer");
+
+const program = `
+const x = a - b /* je suis un commentaire  je dois etre ignoré */    
+const y = -2536
+const _ = 6
+const a = 123
+const b_ = +34
+fn(a,b) { if(a==b) {return (a+b+1356)}}
+`;
+
+const lexer = new Lexer(program);
+
+console.log(lexer.tokenize());
